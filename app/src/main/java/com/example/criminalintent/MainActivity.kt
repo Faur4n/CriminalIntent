81078f27ba14cd025cacc6603a16c6bb5a4b81a4
Владислав Бойцов <38418646+Faur4n@users.noreply.github.com>
2019-11-25 21:22:30 +0300

    Jetpack Navigation Swap
    Succesfully swap to Jetpack Navigation

--- app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -2,36 +2,45 @@ package com.example.criminalintent
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import java.util.*
 
 private const val TAG = "MainActivity"
 
-class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
+class MainActivity : AppCompatActivity()
+   // , CrimeListFragment.Callbacks
+{
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.nav_host_navigation)
 
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+//        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+//
+//        if (currentFragment == null) {
+//            val fragment = CrimeListFragment.newInstance()
+//            supportFragmentManager
+//                .beginTransaction()
+//                .add(R.id.fragment_container, fragment)
+//                .commit()
+//        }
 
-        if (currentFragment == null) {
-            val fragment = CrimeListFragment.newInstance()
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragment_container, fragment)
-                .commit()
-        }
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+        val navController = host.navController
 
 
     }
 
-    override fun OnCrimeSelected(crimeId: UUID) {
-        val fragment = CrimeFragment.newInstance(crimeId)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .addToBackStack(null)
-            .commit()
-    }
+
+
+//    override fun OnCrimeSelected(crimeId: UUID) {
+//        val fragment = CrimeFragment.newInstance(crimeId)
+//        supportFragmentManager
+//            .beginTransaction()
+//            .replace(R.id.fragment_container, fragment)
+//            .addToBackStack(null)
+//            .commit()
+//    }
 }
